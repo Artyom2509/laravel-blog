1783b6cda0c126cc2aec33c93be2c75002d74dfb
Artyom2509 <bicepssapiens228@gmail.com>
2020-10-27 19:11:36 +0600

    Initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
+
+class Post extends Model
+{
+    use HasFactory;
+    use Sluggable;
+
+    protected $fillable = ['title', 'content'];
+
+    const IS_DRAFT = 0;
+    const IS_PUBLIC = 1;
+    const IS_STANDART = 0;
+    const IS_FEATURED = 1;
+
+    public function category()
+    {
+        return $this->hasOne(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tags',
+            'post_id',
+            'tag_id'
+        );
+    }
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public static function add($fields)
+    {
+        $post = new self;
+        $post->fill($fields);
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        // удалить картинку поста
+        $this->delete();
+    }
+
+    public function uploadImage($image)
+    {
+        if ($image == null) {return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getImage()
+    {
+        if ($this->image == null) {
+            return "/img/no_image.png";
+        }
+
+        return "/uploads/$this->image";
+    }
+
+    public function setCategory($id)
+    {
+        if ($id == null) {return; }
+
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    public function setTags($ids)
+    {
+        if ($ids == null) {return; }
+
+        $this->tags()->sync($ids);
+    }
+
+    public function setDraft()
+    {
+        $this->status = Post::IS_DRAFT;
+        $this->save();
+    }
+
+    public function setPublic()
+    {
+        $this->status = Post::IS_PUBLIC;
+        $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+        if ($value == null) {
+            return $this->setDraft();
+        }
+
+        return $this->setPublic();
+    }
+
+    public function setFeatured()
+    {
+        $this->is_featured = Post::IS_FEATURED;
+        $this->save();
+    }
+
+    public function setStandart()
+    {
+        $this->is_featured = Post::IS_STANDART;
+        $this->save();
+    }
+
+    public function toggleFeatured($value)
+    {
+        if ($value == null) {
+            return $this->setStandart();
+        }
+
+        return $this->setFeatured();
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Storage;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    const IS_ADMIN = 1;
+    const IS_NORMAL = 0;
+    const IS_BANNED = 1;
+    const IS_ACTIVE = 0;
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public static function add($fields)
+    {
+        $user = new self;
+        $user->fill($fields);
+        $user->password = bcript($fields['password']);
+        $user->save();
+
+        return $user;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->password = bcript($fields['password']);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        Storage::delete('uploads/' . $this->image);
+        $this->delete();
+    }
+    public function uploadAvatar($image)
+    {
+        if ($image == null) {return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getAvatar()
+    {
+        if ($this->image == null) {
+            return "/img/no_avatar.png";
+        }
+
+        return "/uploads/$this->image";
+    }
+
+    public function makeAdmin()
+    {
+        $this->is_admin = User::IS_ADMIN;
+        $this->save();
+    }
+
+    public function makeNormal()
+    {
+        $this->is_admin = User::IS_NORMAL;
+        $this->save();
+    }
+
+    public function toggleAdmin($value)
+    {
+        if ($value == null) {
+            return $this->makeNormal();
+        }
+
+        return $this->makeAdmin();
+    }
+
+    public function ban()
+    {
+        $this->status = User::IS_BANNED;
+        $this->save();
+    }
+
+    public function unban()
+    {
+        $this->status = User::IS_ACTIVE;
+        $this->save();
+    }
+
+    public function toggleBan($value)
+    {
+        if ($value == null) {
+            return $this->unban();
+        }
+
+        return $this->ban();
+    }
+}
